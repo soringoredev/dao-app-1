b3a9a2063050e13e6024a9f59b4ce9258eccb463
Sorin Gore <soringoredev@gmail.com>
2025-06-02 17:44:08 +0300

    feat: Add save method

---
+++ src/main/java/com/example/crud_app/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.example.crud_app.dao;
+
+import com.example.crud_app.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+}
---
+++ src/main/java/com/example/crud_app/dao/StudentDAOImplementation.java
@@ -0,0 +1,26 @@
+package com.example.crud_app.dao;
+
+import com.example.crud_app.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImplementation implements StudentDAO {
+
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public StudentDAOImplementation(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+
+    }
+
+}
